250feacb1f63eb387d451af05c65dd2a10805344
Ondrej Hrebicek <ondrej@iterative.ly>
2020-03-02 23:22:39 -0800

    Create itly.rb

---
+++ HomebrewFormula/itly.rb
@@ -0,0 +1,18 @@
+class Itly < Formula
+  desc "The Itly CLI"
+  homepage "https://iterative.ly/"
+  url "https://cli-assets.iterative.ly/itly-v0.0.49/itly-v0.0.49.tar.gz"
+  sha256 "c507cf121604f70b45b738aa31bcc58a10805ec5afebe47cb192f43715b29566"
+
+  depends_on "node"
+
+  def install
+    inreplace "bin/itly", /^CLIENT_HOME=/, "export ITLY_OCLIF_CLIENT_HOME=#{lib/"client"}\nCLIENT_HOME="
+    libexec.install Dir["*"]
+    bin.install_symlink libexec/"bin/itly" => "itly"
+  end
+
+  test do
+    system bin/"itly", "version"
+  end
+end
