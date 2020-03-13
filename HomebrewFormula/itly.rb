1f1f3ab04d649012290e698ba602844f3467c139
Ondrej Hrebicek <ondrej@iterative.ly>
2020-03-13 08:27:23 -0700

    Update itly.rb

--- HomebrewFormula/itly.rb
+++ HomebrewFormula/itly.rb
@@ -1,8 +1,8 @@
 class Itly < Formula
   desc "The Itly CLI"
   homepage "https://iterative.ly/"
-  url "https://cli-assets.iterative.ly/itly-v0.0.51/itly-v0.0.51.tar.gz"
-  sha256 "d69e43278bb45d494e2d97dd2a86efdf0315d209c31f1f76bbade0c4fd26972d"
+  url "https://cli-assets.iterative.ly/itly-v0.0.52/itly-v0.0.52.tar.gz"
+  sha256 "06ecfb914c08fbfacc3eaa3bbdbf53a6d6a8ff435cf54ed095fd14b101e307c4"
 
   depends_on "node"
 
