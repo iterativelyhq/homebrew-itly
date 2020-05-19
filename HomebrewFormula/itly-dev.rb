3732407d1818239e0f023cf9d4f48bd27f123888
CLI Deploy <support@iterative.ly>
2020-05-19 02:01:42 +0000

    Updated formula for 0.0.56-build.446

--- HomebrewFormula/itly-dev.rb
+++ HomebrewFormula/itly-dev.rb
@@ -1,8 +1,8 @@
 class ItlyDev < Formula
   desc "The Itly CLI (Dev)"
   homepage "https://iterative.ly/"
-  url "https://cli-assets-dev.iterative.ly/channels/build/itly-v0.0.56-build.445/itly-v0.0.56-build.445.tar.gz"
-  sha256 "c548cc6f0ca4dc05d875c1ec6a1b713c5f930f10e8f132bce3d040bcca3c3814"
+  url "https://cli-assets-dev.iterative.ly/channels/build/itly-v0.0.56-build.446/itly-v0.0.56-build.446.tar.gz"
+  sha256 "4c9a7138ebafd838a2afb568c742256a918b439e1ade6b3d309cbe56c71d289c"
 
   depends_on "node"
 
