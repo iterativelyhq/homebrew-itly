4c3d3bd9deb05e07ecc94f67f256baffef7f8ed8
Justin Fiedler <60665705+justin-iteratively@users.noreply.github.com>
2020-06-18 12:16:52 -0700

    Changed back to "node" from "node@12"

--- HomebrewFormula/itly-dev.rb
+++ HomebrewFormula/itly-dev.rb
@@ -4,7 +4,7 @@ class ItlyDev < Formula
   url "https://cli-assets-dev.iterative.ly/channels/build/itly-v0.0.56-build.466/itly-v0.0.56-build.466.tar.gz"
   sha256 "cc6760c805a96b4b26fc8f0affd67a8e1000fca5d154506f1492a37a05a4ec2e"
 
-  depends_on "node@12"
+  depends_on "node"
 
   def install
     inreplace "bin/itly", /^CLIENT_HOME=/, "export ITLY_OCLIF_CLIENT_HOME=#{lib/"client"}\nCLIENT_HOME="
