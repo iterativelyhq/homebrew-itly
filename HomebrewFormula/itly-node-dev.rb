ec831ae4026469926da4611cfb6a78393b663b7a
Justin Fiedler <justin@iterative.ly>
2021-02-05 21:13:31 +0000

    [ITLY-1568] libexec install itly-node-dev

--- HomebrewFormula/itly-node-dev.rb
+++ HomebrewFormula/itly-node-dev.rb
@@ -5,7 +5,7 @@ class ItlyNodeDev < Formula
   sha256 "3be1a9fb2715f62561912d6e824f5e8dc2d64cf7e074f6c4ac8b5c9b4a836761"
 
   def install
-    # share.install buildpath/"node"
+    libexec.install buildpath/"node"
   end
 
   # def test
