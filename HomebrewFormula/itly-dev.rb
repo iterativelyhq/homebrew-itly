384e189470090c0f7cd871318bcbc41572a2bbf9
Justin Fiedler <justin@iterative.ly>
2021-02-05 22:36:07 +0000

    [ITLY-1568] Updated itly-dev to use itly-node-dev.libexec node binary

--- HomebrewFormula/itly-dev.rb
+++ HomebrewFormula/itly-dev.rb
@@ -8,7 +8,7 @@ class ItlyDev < Formula
 
   def install
     inreplace "bin/itly", /^CLIENT_HOME=/, "export ITLY_OCLIF_CLIENT_HOME=#{lib/"client"}\nCLIENT_HOME="
-    inreplace "bin/itly", "\"$DIR/node\"", "#{Formula["itly-node-dev"].opt_share}/node"
+    inreplace "bin/itly", "\"$DIR/node\"", "#{Formula["itly-node-dev"].libexec}/node"
     libexec.install Dir["*"]
     bin.install_symlink libexec/"bin/itly" => "itly-dev"
   end
