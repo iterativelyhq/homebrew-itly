0f15bb55c5772663c5f57b284147adc438686be5
Justin Fiedler <justin@iterative.ly>
2021-02-05 20:37:21 +0000

    [ITLY-1568] No longer install itly-node-dev to /usr/local/share

--- HomebrewFormula/itly-node-dev.rb
+++ HomebrewFormula/itly-node-dev.rb
@@ -4,9 +4,9 @@ class ItlyNodeDev < Formula
   url "https://cli-assets-dev.iterative.ly/node/node-12.16.2.tar.xz"
   sha256 "3be1a9fb2715f62561912d6e824f5e8dc2d64cf7e074f6c4ac8b5c9b4a836761"
 
-  def install
-    share.install buildpath/"node"
-  end
+#   def install
+#     share.install buildpath/"node"
+#   end
 
   # def test
   #   system bin/"node", "version"
