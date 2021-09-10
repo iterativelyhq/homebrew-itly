153c5c827c24811b8aeada0dca59980ed561961d
CLI Deploy <support@iterative.ly>
2021-09-10 17:53:42 +0000

    Updated formula for 1.2.1-itly-41640.878

--- HomebrewFormula/itly-dev.rb
+++ HomebrewFormula/itly-dev.rb
@@ -1,13 +1,13 @@
 class ItlyDev < Formula
   desc "The Itly CLI (Dev)"
   homepage "https://iterative.ly/"
-  url "https://cli-assets-dev.iterative.ly/channels/itly-41640/ampli-v1.2.1-itly-41640.877/ampli-v1.2.1-itly-41640.877.tar.gz"
-  sha256 "1a99fb5e35bd90644285027ab763a377c26416a4a89b855d7b2973d56c501f29"
+  url "https://cli-assets-dev.iterative.ly/channels/itly-41640/ampli-v1.2.1-itly-41640.878/ampli-v1.2.1-itly-41640.878.tar.gz"
+  sha256 "919611f2b2c2766e2dadb7d72834539ed546611eeba40e2d241f11dfd958261f"
 
   depends_on "iterativelyhq/itly/itly-node-dev"
 
   def install
-    inreplace "bin/ampli"", /^CLIENT_HOME=/, "export ITLY_OCLIF_CLIENT_HOME=#{lib/"client"}\nCLIENT_HOME="
+    inreplace "bin/ampli", /^CLIENT_HOME=/, "export ITLY_OCLIF_CLIENT_HOME=#{lib/"client"}\nCLIENT_HOME="
     inreplace "bin/ampli", "\"$DIR/node\"", "#{Formula["itly-node-dev"].libexec}/node"
     libexec.install Dir["*"]
     bin.install_symlink libexec/"bin/ampli" => "itly-dev"
