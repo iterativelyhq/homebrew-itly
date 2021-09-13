0e8f96de0b1105405527219a37c5a0575ea2eea9
CLI Deploy <support@iterative.ly>
2021-09-13 06:28:19 +0000

    Updated formula for 1.2.1-itly-41640.880

--- HomebrewFormula/itly-dev.rb
+++ HomebrewFormula/itly-dev.rb
@@ -1,8 +1,8 @@
 class ItlyDev < Formula
   desc "The Itly CLI (Dev)"
   homepage "https://iterative.ly/"
-  url "https://cli-assets-dev.iterative.ly/channels/itly-41640/ampli-v1.2.1-itly-41640.879/ampli-v1.2.1-itly-41640.879.tar.gz"
-  sha256 "d5cd8853bf84b23420ed4f4856f2c33de9ea5aa529af7fce7217c2c26c1f8048"
+  url "https://cli-assets-dev.iterative.ly/channels/itly-41640/ampli-v1.2.1-itly-41640.880/ampli-v1.2.1-itly-41640.880.tar.gz"
+  sha256 "9fe418a2b34d702633698e930fbadeea94c49cd8d959145ab057dcd3956f66d6"
 
   depends_on "iterativelyhq/itly/itly-node-dev"
 
